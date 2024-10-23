5ea3578dbd6e540658e2a52c41693ce456d0d77d
Ammar <123098233+Ammar-XD@users.noreply.github.com>
2024-10-23 20:23:49 +0530

    Update - Database

--- src/main/java/dev/infnoxx/region/Region.java
+++ src/main/java/dev/infnoxx/region/Region.java
@@ -1,6 +1,11 @@
 package dev.infnoxx.region;
 
+import dev.infnoxx.region.database.MySQLConnectionPool;
+import dev.infnoxx.region.gui.GUIManager;
+import dev.infnoxx.region.region.RegionManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
 
 public final class Region extends JavaPlugin {
 
--- src/main/java/dev/infnoxx/region/database/MySQLConnectionPool.java
+++ src/main/java/dev/infnoxx/region/database/MySQLConnectionPool.java
@@ -1,4 +1,33 @@
 package dev.infnoxx.region.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MySQLConnectionPool {
+
+    private HikariDataSource dataSource;
+
+    public MySQLConnectionPool(FileConfiguration config) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(config.getString("database.url"));
+        hikariConfig.setUsername(config.getString("database.username"));
+        hikariConfig.setPassword(config.getString("database.password"));
+        hikariConfig.setMaximumPoolSize(10);
+
+        this.dataSource = new HikariDataSource(hikariConfig);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public void close() {
+        if (dataSource != null) {
+            dataSource.close();
+        }
+    }
 }
